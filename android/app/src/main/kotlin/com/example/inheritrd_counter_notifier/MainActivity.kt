c820379849b3b8fd2f73545d04be0b68780fed5e
Sneha Canopas <sneha.s@canopas.com>
2023-06-07 11:55:24 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/inheritrd_counter_notifier/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.inheritrd_counter_notifier
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/counter.dart
@@ -0,0 +1,5 @@
+class Counter{
+  final int count;
+  Counter({required this.count});
+}
+
